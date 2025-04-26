108d0f24c9e1741ef5af28e13308d90e64ecbf2a
jocker18291 <dawidans714@gmail.com>
2025-04-26 23:28:46 +0200

    Add edge function declared

--- graphMatrix.cpp
+++ graphMatrix.cpp
@@ -10,4 +10,9 @@ public:
         vertices = v;
         matrix.resize(vertices, std::vector<int>(vertices, 0)); //vertices x vertices matrix filled with 0s
     }
+
+    void addEdge(int src, int dest, int weight = 1) {
+        matrix[src][dest] = weight;
+        matrix[dest][src] = weight; //for non-oriented graph
+    }
 };
