f8fa594d182a3f995b9baf1b6f0f8cb89c2f0320
jocker18291 <dawidans714@gmail.com>
2025-04-26 23:31:03 +0200

    printMatrix function declared

--- graphMatrix.cpp
+++ graphMatrix.cpp
@@ -15,4 +15,13 @@ public:
         matrix[src][dest] = weight;
         matrix[dest][src] = weight; //for non-oriented graph
     }
+
+    void printMatrix() const {
+        for(const auto& row : matrix) {
+            for (int val : row) {
+                std::cout << val << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 };
