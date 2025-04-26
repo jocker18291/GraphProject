01a594b279427ff67ee7dae2085a6a651ea3ca79
jocker18291 <dawidans714@gmail.com>
2025-04-26 23:16:42 +0200

    Add private properties and constructor

--- graphMatrix.cpp
+++ graphMatrix.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 #include <vector>
+
+class graphMatrix {
+private:
+    int vertices;
+    std::vector<std::vector<int>> matrix;
+public:
+    graphMatrix(int v) {
+        vertices = v;
+        matrix.resize(vertices, std::vector<int>(vertices, 0));
+    }
+};
