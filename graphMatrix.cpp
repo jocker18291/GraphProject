a15355b568d5c3bca88114ce1ff8c118cffcbc07
jocker18291 <dawidans714@gmail.com>
2025-04-27 10:58:45 +0200

    Changed the layout

--- graphMatrix.cpp
+++ graphMatrix.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 #include <vector>
+#include "graphMatrix.hpp"
 
-class graphMatrix {
-private:
-    int vertices;
-    std::vector<std::vector<int>> matrix;
-public:
-    graphMatrix(int v) {
-        vertices = v;
-        matrix.resize(vertices, std::vector<int>(vertices, 0)); //vertices x vertices matrix filled with 0s
-    }
+graphMatrix::graphMatrix(int v) {
+    vertices = v;
+    matrix.resize(vertices, std::vector<int>(vertices, 0)); //vertices x vertices matrix filled with 0s
+}
 
-    void addEdge(int src, int dest, int weight = 1) {
-        matrix[src][dest] = weight;
-        matrix[dest][src] = weight; //for non-oriented graph
-    }
+void graphMatrix::addEdge(int src, int dest, int weight = 1) {
+    matrix[src][dest] = weight;
+    matrix[dest][src] = weight; //for non-oriented graph
+}
 
-    void printMatrix() const {
-        for(const auto& row : matrix) {
-            for (int val : row) {
-                std::cout << val << " ";
-            }
-            std::cout << std::endl;
+void graphMatrix::printMatrix() const {
+    for(const auto& row : matrix) {
+        for (int val : row) {
+            std::cout << val << " ";
         }
+        std::cout << std::endl;
     }
-};
+}
--- graphMatrix.hpp
+++ graphMatrix.hpp
@@ -1,4 +1,15 @@
 #ifndef GRAPH_MATRIX_HPP
 #define GRAPH_MATRIX_HPP
+#include <vector>
+
+class graphMatrix {
+private:
+    int vertices;
+    std::vector<std::vector<int>> matrix;
+public:
+    graphMatrix(int v);
+    void addEdge(int src, int dest, int weight = 1);
+    void printMatrix() const;
+};
 
 #endif
