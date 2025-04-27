2b845638a7f5bef1ceea0d24ef28a33855592566
jocker18291 <dawidans714@gmail.com>
2025-04-27 10:37:45 +0200

    Add constructor

--- graphList.cpp
+++ graphList.cpp
@@ -6,4 +6,9 @@ class graphList {
 private:
     int vertices;
     std::vector<std::list<std::pair<int, int>>> adjList;
+public:
+    graphList(int v) {
+        vertices = v;
+        adjList.resize(vertices);
+    }
 };
