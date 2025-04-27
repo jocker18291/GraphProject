cdd3828e0fb1a0acbd80077ac031a432078d3d91
jocker18291 <dawidans714@gmail.com>
2025-04-27 10:43:00 +0200

    Add two methods

--- graphList.cpp
+++ graphList.cpp
@@ -11,4 +11,19 @@ public:
         vertices = v;
         adjList.resize(vertices);
     }
+
+    void addEdge(int src, int dest, int weight = 1) {
+        adjList[src].push_back({dest, weight});
+        adjList[dest].push_back({src, weight});
+    }
+
+    void printList() const {
+        for(int i = 0; i < vertices; ++i) {
+            std::cout << i << ": ";
+            for(const auto& neighbor : adjList[i]) {
+                std::cout << "(" <<neighbor.first << ", " << neighbor.second << ")";
+            }
+            std::cout << std::endl;
+        }
+    }
 };
