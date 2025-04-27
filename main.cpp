58a4866b4283fd468041a084b4169fc3ed854799
jocker18291 <dawidans714@gmail.com>
2025-04-27 11:30:49 +0200

    Another test

--- main.cpp
+++ main.cpp
@@ -7,5 +7,10 @@ int main() {
     gm.addEdge(0, 1, 2);
     gm.addEdge(0, 4);
     gm.printMatrix();
+
+    graphList gl(5);
+    gl.addEdge(0, 1, 2);
+    gl.addEdge(0, 4);
+    gl.printList();
     return 0;
 }
