f9d07d65b4516da5bcceef134690b4e3cc59d8d0
jocker18291 <dawidans714@gmail.com>
2025-04-27 11:05:48 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+#include "graphList.hpp"
+#include "graphMatrix.hpp"
+
+int main() {
+    graphMatrix gm(5);
+    gm.addEdge(0, 1, 2);
+    gm.addEdge(0, 4);
+    gm.printMatrix();
+    return 0;
+}
