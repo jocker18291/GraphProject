e30f4a7c7f97441e181a2caf96f7cf326a23e77d
jocker18291 <dawidans714@gmail.com>
2025-04-27 11:03:26 +0200

    Changed the layout

--- graphList.cpp
+++ graphList.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include "graphList.hpp"
 
-class graphList {
-private:
-    int vertices;
-    std::vector<std::list<std::pair<int, int>>> adjList;
-public:
-    graphList(int v) {
-        vertices = v;
-        adjList.resize(vertices);
-    }
+graphList::graphList(int v) {
+    vertices = v;
+    adjList.resize(vertices);
+}
 
-    void addEdge(int src, int dest, int weight = 1) {
-        adjList[src].push_back({dest, weight});
-        adjList[dest].push_back({src, weight});
-    }
+void graphList::addEdge(int src, int dest, int weight = 1) {
+    adjList[src].push_back({dest, weight});
+    adjList[dest].push_back({src, weight});
+}
 
-    void printList() const {
-        for(int i = 0; i < vertices; ++i) {
-            std::cout << i << ": ";
-            for(const auto& neighbor : adjList[i]) {
-                std::cout << "(" <<neighbor.first << ", " << neighbor.second << ")";
-            }
-            std::cout << std::endl;
+void graphList::printList() const {
+    for(int i = 0; i < vertices; ++i) {
+        std::cout << i << ": ";
+        for(const auto& neighbor : adjList[i]) {
+            std::cout << "(" <<neighbor.first << ", " << neighbor.second << ")";
         }
+        std::cout << std::endl;
     }
-};
+}
--- graphList.hpp
+++ graphList.hpp
@@ -1,4 +1,16 @@
 #ifndef GRAPH_LIST_HPP
 #define GRAPH_LIST_HPP
+#include <vector>
+#include <list>
+
+class graphList {
+private:
+    int vertices;
+    std::vector<std::list<std::pair<int, int>>> adjList;
+public:
+    graphList(int v);
+    void addEdge(int src, int dest, int weight = 1);
+    void printList() const;
+};
 
 #endif
