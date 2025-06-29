fb4dd63709f8da89d13438c214a48803497078d4
jocker18291 <dawidans714@gmail.com>
2025-06-29 21:10:26 +0200

    Moved header files to a directory

---
+++ include/graphList.hpp
@@ -0,0 +1,19 @@
+#ifndef GRAPH_LIST_HPP
+#define GRAPH_LIST_HPP
+#include <vector>
+#include <list>
+
+class graphList {
+private:
+    int vertices;
+    std::vector<std::list<std::pair<int, int>>> adjList;
+public:
+    graphList(int v);
+    void addEdge(int src, int dest, int weight = 1);
+    void printList() const;
+    void BFS(int start) const;
+    std::vector<int> Dijkstra(int start) const;
+    std::vector<int> BellmanFord(int start) const;
+};
+
+#endif
